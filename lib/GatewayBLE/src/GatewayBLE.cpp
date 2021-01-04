6ff55142b5cea1c4b8db4e8bfcd3af6ba3beb8db
Joseph Howarth <josephhowarth17@gmail.com>
2021-01-04 16:14:33 -0600

    Even more progress on refactoring...

--- lib/GatewayBLE/src/GatewayBLE.cpp
+++ lib/GatewayBLE/src/GatewayBLE.cpp
@@ -13,7 +13,7 @@ GatewayBLE::GatewayBLE(){
 
     //Register all the callbacks.
     BLE.onDisconnected(GatewayBLE::disconnectCallback,this);
-    BLE.onConnected(GatewayBLE::connectedCallback,this);
+    //BLE.onConnected(GatewayBLE::connectedCallback,this);  //We are not using this yet.
 
     dataCharcteristic.onDataReceived(bleRxCallback,NULL);
 
@@ -24,14 +24,70 @@ void GatewayBLE::startBLE(){
     BLE.on();
 }
 
-void GatewayBLE::scanBLE(size_t timeout){
+int GatewayBLE::connectBLE(){
+
+    int numDevicesFound = foundDevices.size();
+    Log.info("Connecting to %d  BT devices ...",numDevicesFound);
+    
+    //Try and connect to each device.
+    for(int i = 0; i < numDevicesFound; i++){
+
+        particle::BlePeerDevice newConnection;
+        newConnection = BLE.connect(foundDevices[i].addr,false); //Blocking Function.
+
+        if(newConnection.connected()){
+            
+            Log.info("Connected to BT device: %s", foundDevices[i].name);
+            
+
+        }
+    }
+}
+
+int GatewayBLE::scanBLE(size_t timeout){
 
     BLE.setScanTimeout(timeout);
-    BLE.scan(scanResultCallback, this);
+    return BLE.scan(scanResultCallback, this);
 }
 
 void GatewayBLE::bleRxCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context){}
 void GatewayBLE::disconnectCallback(const BlePeerDevice& peer, void* context){}
 void GatewayBLE::connectedCallback(const BlePeerDevice& peer, void* context){}
-void GatewayBLE::scanResultCallback(const BleScanResult *scanResult, void *context){}
+
+void GatewayBLE::scanResultCallback(const BleScanResult *scanResult, void *context){
+    
+    //Get our BLE object, since we set this pointer when we registered the callback.
+    GatewayBLE * gatewayBLE = (GatewayBLE *) context;
+
+    Log.info("MAC: %02X:%02X:%02X:%02X:%02X:%02X | RSSI: %dBm",
+    scanResult->address[0], scanResult->address[1], scanResult->address[2],
+    scanResult->address[3], scanResult->address[4], scanResult->address[5], scanResult->rssi);
+
+
+    String name = scanResult->advertisingData.deviceName();
+    if (name.length() > 0) {
+        Log.info("deviceName: %s", name.c_str());
+    }
+
+    //Check the device name we just found against our approved list.
+    for(int i=0; i< BLE_MAX_CONNECTION; i++){
+
+        if(strcmp(name,gatewayBLE->approvedDevices[i]) ==0 ){
+
+            bleConnection_t connection;
+            connection.addr = scanResult->address;
+            connection.name = name;
+
+            gatewayBLE->foundDevices.push_back(connection);
+        }
+    }
+
+    // if(strcmp(name,"G02_A") ==0 || strcmp(name,"G02_B") ==0 || strcmp(name, "G02_C")==0){
+    //     //Save the address of the device with name G02.
+    //     //And notify the main loop.
+    //     btDeviceFound = true;
+    //     btAddr = scanResult->address;
+
+    // }
+}
         
--- lib/GatewayBLE/src/GatewayBLE.h
+++ lib/GatewayBLE/src/GatewayBLE.h
@@ -7,6 +7,17 @@
 
 #define SERVICE_UUID            "7abd7d09-dabd-4b5d-882d-7f4e5096f8f9"
 #define CHARACTERISTIC_UUID     0xe9,0xa4,0x19,0x3d,0x4d,0x05,0x45,0xf9,0x8b,0xc2,0x91,0x15,0x78,0x6c,0x96,0xc2
+#define BLE_MAX_CONNECTION      4
+
+//This groups useful info about a connection together.
+typedef struct{
+
+    String name;
+    particle::BleAddress addr;
+    particle::BlePeerDevice conn;
+
+} bleConnection_t;
+
 
 class GatewayBLE{
 
@@ -19,32 +30,46 @@ class GatewayBLE{
         void startBLE();
 
         //Starts a BLE scan. Blocks for timeout (seconds?), then the callback is called with the results.
-        void scanBLE(size_t timeout=5);
+        //Returns the number of devices found.
+        int scanBLE(size_t timeout=5);
+
+        //This will connect to all devices with the correct name. Should be called after the scan function is called.
+        //Returns the number of devices that were connected to.
+        int connectBLE();
+
+
+        std::vector<bleConnection_t> connectedNodes;  //Handle for the BLE connections.
+        uint8_t numConnections;
 
     private:
-    
-    BleUuid serviceUuid; 
-    BleUuid characteristicUuid;
 
-    BleCharacteristic dataCharcteristic; 
+        //The value of these is from the defined constants at the top of the file.
+        BleUuid serviceUuid; 
+        BleUuid characteristicUuid;
 
-    std::vector<particle::BlePeerDevice> connectedNodes;  //Handle for the BLE connections.
-    uint8_t numConnections;
+        //Used for getting data from the sensor nodes.
+        BleCharacteristic dataCharcteristic; 
 
-    //Callbacks 
-    //These are called on the BLE stack, so don't do long operations (delay,etc) or use tons of memory.
+        //We will try and connect to devices with these names.
+        const String approvedDevices[BLE_MAX_CONNECTION] = {"G02_A","G02_B","G02_C","G02_D"};
 
-        //Called when data is received.
-        static void bleRxCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context);
+        //For scanning.
+        std::vector<bleConnection_t> foundDevices;  
 
-        //Called when a device connectes to the gateway.
-        static void connectedCallback(const BlePeerDevice& peer, void* context);
+        //Callbacks 
+        //These are called on the BLE stack, so don't do long operations (delay,etc) or use tons of memory.
 
-        //Called when a device disconnects from the gateway.
-        static void disconnectCallback(const BlePeerDevice& peer, void* context);
+            //Called when data is received.
+            static void bleRxCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context);
 
-        //Called when the BLE scan results are in.
-        static void scanResultCallback(const BleScanResult *scanResult, void *context);
+            //Called when a device connectes to the gateway.
+            static void connectedCallback(const BlePeerDevice& peer, void* context);
+
+            //Called when a device disconnects from the gateway.
+            static void disconnectCallback(const BlePeerDevice& peer, void* context);
+
+            //Called when the BLE scan results are in.
+            static void scanResultCallback(const BleScanResult *scanResult, void *context);
 
 
 };
