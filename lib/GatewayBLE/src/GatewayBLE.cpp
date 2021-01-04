61896d5b36aea67d4c69c1b2e7c1197ec7f86f78
Joseph Howarth <josephhowarth17@gmail.com>
2021-01-04 15:23:24 -0600

    More progress on GatewayBLE class. Not itegrated...

--- lib/GatewayBLE/src/GatewayBLE.cpp
+++ lib/GatewayBLE/src/GatewayBLE.cpp
@@ -1,2 +1,37 @@
 #include "GatewayBLE.h"
 
+GatewayBLE::GatewayBLE(){
+
+    numConnections = 0;
+
+    //Setup up the service and characteristic UUIDs.
+    serviceUuid = BleUuid(SERVICE_UUID);
+
+    const char dataUUID [16] = {CHARACTERISTIC_UUID};
+    characteristicUuid = BleUuid((uint8_t *)dataUUID,BleUuidOrder::LSB); //LSB must be specified since that is how the ItsyBitsy specifies it.
+
+
+    //Register all the callbacks.
+    BLE.onDisconnected(GatewayBLE::disconnectCallback,this);
+    BLE.onConnected(GatewayBLE::connectedCallback,this);
+
+    dataCharcteristic.onDataReceived(bleRxCallback,NULL);
+
+}
+
+void GatewayBLE::startBLE(){
+
+    BLE.on();
+}
+
+void GatewayBLE::scanBLE(size_t timeout){
+
+    BLE.setScanTimeout(timeout);
+    BLE.scan(scanResultCallback, this);
+}
+
+void GatewayBLE::bleRxCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context){}
+void GatewayBLE::disconnectCallback(const BlePeerDevice& peer, void* context){}
+void GatewayBLE::connectedCallback(const BlePeerDevice& peer, void* context){}
+void GatewayBLE::scanResultCallback(const BleScanResult *scanResult, void *context){}
+        
--- lib/GatewayBLE/src/GatewayBLE.h
+++ lib/GatewayBLE/src/GatewayBLE.h
@@ -2,17 +2,51 @@
 #define GATEWAY_BLE_H
 
 #include "ble_hal.h"
+#include "Particle.h"
 #include <vector>
+
+#define SERVICE_UUID            "7abd7d09-dabd-4b5d-882d-7f4e5096f8f9"
+#define CHARACTERISTIC_UUID     0xe9,0xa4,0x19,0x3d,0x4d,0x05,0x45,0xf9,0x8b,0xc2,0x91,0x15,0x78,0x6c,0x96,0xc2
 
 class GatewayBLE{
 
     public:
 
+        //Constructor
+        GatewayBLE();
 
+        //Turns the BT radio on.
+        void startBLE();
+
+        //Starts a BLE scan. Blocks for timeout (seconds?), then the callback is called with the results.
+        void scanBLE(size_t timeout=5);
 
     private:
     
-        
+    BleUuid serviceUuid; 
+    BleUuid characteristicUuid;
+
+    BleCharacteristic dataCharcteristic; 
+
+    std::vector<particle::BlePeerDevice> connectedNodes;  //Handle for the BLE connections.
+    uint8_t numConnections;
+
+    //Callbacks 
+    //These are called on the BLE stack, so don't do long operations (delay,etc) or use tons of memory.
+
+        //Called when data is received.
+        static void bleRxCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context);
+
+        //Called when a device connectes to the gateway.
+        static void connectedCallback(const BlePeerDevice& peer, void* context);
+
+        //Called when a device disconnects from the gateway.
+        static void disconnectCallback(const BlePeerDevice& peer, void* context);
+
+        //Called when the BLE scan results are in.
+        static void scanResultCallback(const BleScanResult *scanResult, void *context);
+
+
 };
 
 
