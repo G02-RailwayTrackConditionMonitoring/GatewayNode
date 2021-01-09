dafc2ee45022f9a27ff88325a5a74835bd5ccda1
Sanjay Abraham <sabraham@arm-corp.ca>
2021-01-09 01:31:25 -0600

    setup_gcp_hook

--- src/GatewayNode.ino
+++ src/GatewayNode.ino
@@ -1,9 +1,3 @@
-/*
- * Project GatewayNode
- * Description:
- * Author:
- * Date:
- */
 
 //The system mode changes how the cloud connection is managed.
 //In Semi-Automatic mode, we must initiate the connection and then it is managed by the Particle firmware.
@@ -13,35 +7,25 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 //This sets the log level for logging over USB.
 SerialLogHandler logHandler(LOG_LEVEL_WARN, {{"app", LOG_LEVEL_TRACE}});
 
-//Public Functions and vairables.
-int blink(String params);
-int numButtonPress;
-
-//Setup the input and output pins.
+int accel = 0;
 int buttonPin = D5;
 int ledPin = D4;
-
-//Gloabl variables
 bool runBlink = false;
+bool publishSampleData = true;
+String data = "";
+String STATION_A = "A";
+String STATION_B = "B";
 
-// setup() runs once, when the device is first turned on.
 void setup()
 {
 
   Log.info("Starting application setup.");
-
-  // Put initialization like pinMode and begin functions here.
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
 
-  //Register functions and variables with Particle cloud. Doing this before connecting will save cell data.
-  Particle.function("blink", blink);
-  Particle.variable("buttonPress", numButtonPress);
-
-  //Connect to the cloud. This should automatically connect cellular.
   if (!Particle.connected())
   {
-    Particle.connect(); //Only connect if were not already connected.
+    Particle.connect();
   }
 
   if (Particle.connected())
@@ -54,40 +38,30 @@ void setup()
   }
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop()
 {
-  // The core of your code will likely live here.
+
   if (digitalRead(buttonPin) == LOW)
   {
-    numButtonPress++; //Button is active low.
-    Log.trace("Button pressed for the %d time.", numButtonPress);
-  }
-  if (runBlink)
-  {
-
-    digitalWrite(ledPin, !digitalRead(ledPin));
-  }
-
-  delay(1000);
-}
-
-//This function will start or stop blinking the onboard LED.
-int blink(String params)
-{
-
-  if (params == "Start")
-  {
-
-    runBlink = true;
-    return 1;
-  }
-  else if (params == "Stop")
-  {
-
-    runBlink = false;
-    return 1;
+    if (publishSampleData)
+    {
+      accel++;
+      Log.trace("%d", accel);
+      data = String::format(
+          "{\"station_a\":\"A\", \"station_b\":\"B\", \"accel\":%d}", accel);
+      Log.trace("Publishing Data");
+      Log.trace(data);
+      digitalWrite(ledPin, HIGH);
+      Particle.publish("sampleData", data, PRIVATE);
+      publishSampleData = false;
+    }
   }
   else
-    return -1;
+  {
+    digitalWrite(ledPin, LOW);
+    Log.trace("Push Button (D5) to publish data");
+    publishSampleData = true;
+  }
+  Log.trace("******");
+  delay(1000);
 }
