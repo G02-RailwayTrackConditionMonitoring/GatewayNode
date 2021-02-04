7fea1c4551677727f35ab3abc8062199938acfb5
santaseleucid <abraha13@myumanitoba.cagit config --global user.email abraha13@myumanitoba.cagit config --global user.email abraha13@myumanitoba.ca>
2021-02-04 16:32:32 -0600

    workign

--- src/GatewayNode.ino
+++ src/GatewayNode.ino
@@ -1,88 +1,40 @@
-/*
- * Project GatewayNode
- * Description:
- * Author:
- * Date:
- */
-
-
-//The system mode changes how the cloud connection is managed.
-//In Semi-Automatic mode, we must initiate the connection and then it is managed by the Particle firmware.
-//This lets us run code before connecting.
+#include "Arduino.h"
+#include "SPI.h"
 SYSTEM_MODE(SEMI_AUTOMATIC);
-
-//This sets the log level for logging over USB.
 SerialLogHandler logHandler(LOG_LEVEL_WARN, {{"app", LOG_LEVEL_TRACE}});
 
-//Public Functions and vairables.
-int blink(String params); 
-int numButtonPress; 
+#define MOSI D12
+#define MISO D11
+#define CS D14
+#define SCK D13
+#define HANDSHAKE A4
 
-//Setup the input and output pins.
-int buttonPin = D5;
-int ledPin = D4; 
+char buf[32] = "testing data trans";
+char r_buf[32];
 
-//Gloabl variables
-bool runBlink = false;
-
-// setup() runs once, when the device is first turned on.
-void setup() {
-
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(MOSI, OUTPUT);
+  pinMode(CS, OUTPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(MISO, INPUT);
+  pinMode(HANDSHAKE, INPUT);  
   Log.info("Starting application setup.");
-
-  // Put initialization like pinMode and begin functions here.
-  pinMode(buttonPin,INPUT_PULLUP); 
-  pinMode(ledPin, OUTPUT);
-
-  //Register functions and variables with Particle cloud. Doing this before connecting will save cell data.
-  Particle.function("blink",blink);
-  Particle.variable("buttonPress",numButtonPress);
-
-  //Connect to the cloud. This should automatically connect cellular.
-  if(!Particle.connected()){
-    Particle.connect(); //Only connect if were not already connected.
+  SPI.setClockSpeed(1000000);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.begin(CS);
   }
 
-  if(Particle.connected()){
-    Log.info("Connected to the Particle Cloud.");
-  }
-  else{
-    Log.error("Could not connect to the Particle Cloud.");
-  }
-  
-}
-
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
-  if(digitalRead(buttonPin) == LOW){
-     numButtonPress++; //Button is active low.
-     Log.trace("Button pressed for the %d time.",numButtonPress);
-  }
-  if(runBlink){
-
-    digitalWrite(ledPin,!digitalRead(ledPin));
-
-  }
-
+void loop()
+{
+  while(!digitalRead(HANDSHAKE)){}
+  digitalWrite(CS, LOW);
+  SPI.transfer(buf, r_buf, 32, NULL);
+  digitalWrite(CS, HIGH);
+  Serial.printlnf("send buffer, %s \n",buf);
+  delay(200);
+  Serial.printlnf("rcv buffer %s \n",r_buf);
   delay(1000);
 }
-
-//This function will start or stop blinking the onboard LED.
-int blink(String params){
-
-  if(params == "Start"){
-
-    runBlink = true ;
-    return 1;
-  }
-  else if(params == "Stop"){
-
-    runBlink = false;
-    return 1;
-  }
-  else return -1;
-
-}
-
-
