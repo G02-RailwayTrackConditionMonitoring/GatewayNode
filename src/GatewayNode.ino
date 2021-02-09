3f42253fc64e0b6071477ef99da1d9c030b7f2e0
santaseleucid <abraha13@myumanitoba.cagit config --global user.email abraha13@myumanitoba.cagit config --global user.email abraha13@myumanitoba.ca>
2021-02-09 14:45:13 -0600

    integrate

--- src/GatewayNode.ino
+++ src/GatewayNode.ino
@@ -9,20 +9,35 @@ SerialLogHandler logHandler(LOG_LEVEL_WARN, {{"app", LOG_LEVEL_TRACE}});
 #define SCK D13
 #define HANDSHAKE A4
 char buf[6] = "hello";
+char spiSendBuf[32] = "SPI transmission - dummy data";
+
 const size_t READ_BUF_SIZE = 33;
 char readBuf[READ_BUF_SIZE];
 size_t readBufOffset = 0;
+int64_t t = 0; 
 void setup()
 {
+  Serial.println("Starting application setup.");
   Serial.begin(9600);
   waitFor(Serial.isConnected, 30000);
+  //UART
   Serial1.begin(115200, SERIAL_DATA_BITS_8 | SERIAL_STOP_BITS_1 | SERIAL_PARITY_NO); 
-  Serial.println("Starting application setup.");
+  //SPI
+  pinMode(MOSI, OUTPUT);
+  pinMode(CS, OUTPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(MISO, INPUT);
+  pinMode(HANDSHAKE, INPUT);  
+  SPI.setClockSpeed(1000000);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.begin(CS);  
+  t = millis();
 }
 
 void loop()
 {
-  while (Serial1.available())
+  if (Serial1.available())
   {
     if (readBufOffset < READ_BUF_SIZE)
     {
@@ -47,6 +62,17 @@ void loop()
       readBufOffset = 0;
     }
   }
+  if(digitalRead(HANDSHAKE) && (millis() -t >= 1000)){
+    digitalWrite(CS, LOW);
+    SPI.transfer(spiSendBuf, NULL, 32, NULL);
+    digitalWrite(CS, HIGH);
+    Serial.printlnf("send buffer, %s \n",spiSendBuf);
+    //delay(1000);
+    //Serial.printlnf("rcv buffer %s \n",r_buf);
+    //delay(1000);
+    t = millis();
+  }
+  
 }
 
 void processBuffer() {
@@ -56,3 +82,4 @@ void processBuffer() {
     }
     Serial.print("\n");
 }
+
